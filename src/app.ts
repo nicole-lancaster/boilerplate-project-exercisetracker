9102cd8105cf090bfec8159514dbb90bb9705aac
nicole <lancasternj@gmail.com>
2024-02-05 19:37:04 +0000

    removing cookies to use localStorage instead

--- src/app.ts
+++ src/app.ts
@@ -6,11 +6,8 @@ import {
   getAllUsers,
   postExerciseById,
   getExerciseLogById,
-  readCookies,
-  setCookies,
 } from "./app.controllers";
 import bodyParser from "body-parser";
-import cookieParser from "cookie-parser";
 export const app = express();
 
 app.use(cors());
@@ -18,12 +15,9 @@ app.use("/public", express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(cookieParser());
 
 app.get("/", getHtml);
 app.post("/api/users", requestCreateOrSaveUsernameToDb);
 app.get("/api/users", getAllUsers);
 app.post("/api/users/:_id/exercises", postExerciseById);
 app.get("/api/users/:_id/logs", getExerciseLogById);
-app.get("/api/setcookies", setCookies);
-app.get("/api/readcookies", readCookies);
